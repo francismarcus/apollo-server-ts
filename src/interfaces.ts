a9b7b3194cdcd3a934967de982dc04c48b9a93ad
Marcus Francis <marcusmagnusson@outlook.com>
2019-12-30 23:14:40 +0100

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,18 @@
 import { User } from 'types';
+import { Express } from 'express';
+import { ContextFunction } from 'apollo-server-core'
 
 export interface UserInterface extends User {
 	password: string;
 }
 
-export interface Context {
-	user: UserInterface;
+export interface Context extends ContextFunction {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	me: any;
+}
+
+export interface ExpressRequest extends Express.Request {
+	headers: {
+		authorization: string;
+	};
 }
