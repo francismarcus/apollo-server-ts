c356234c243995e4243532bccdc90584995423c2
Marcus Francis <marcusmagnusson@outlook.com>
2020-01-05 17:22:10 +0100

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,27 @@
-import { User } from 'types';
+import { User, Program } from 'types';
 import { Express } from 'express';
-import { ContextFunction } from 'apollo-server-core'
+import { ContextFunction } from 'apollo-server-core';
+import { UserModelType } from './models/User';
+import { ProgramModelType } from './models/Program';
 
 export interface UserInterface extends User {
 	password: string;
 }
 
+export type ProgramInterface = Program;
+
+export interface Models {
+	User: UserModelType;
+	Program: ProgramModelType;
+}
+
+export interface Me {
+	userId: string;
+}
 export interface Context extends ContextFunction {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	me: any;
+	me: Me;
+	models: Models;
 }
 
 export interface ExpressRequest extends Express.Request {
